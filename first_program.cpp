66621cc3f4017321b120a62f59c01e2e3d955d53
geethakotari891 <147472827+geethakotari891@users.noreply.github.com>
2024-02-14 06:28:16 +0530

    Add files via upload

---
+++ first_program.cpp
@@ -0,0 +1,6 @@
+#include<iostream>   //preprocessor directive
+  //using namespace std;  -> import the entity of std namespace to current namespace
+int main (){  //main function
+    std::cout<<"amma"<<std::endl;  //print 
+    return 0;  //completion of the function
+}
